0f220c6418d15026eb95ca9f66acf177445f627f
David Denise <Denise.David@eoc.ch>
2020-07-23 10:26:20 +0200

    pre merge

---
+++ src/component/domandeListaATendina/domandeListaATendina.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import { useSelector } from 'react-redux';
+import useStyles from './style';
+import { selectData } from '../../store/slice/domandeSlice';
+import ListaATendina from '../ListaATendina/ListaATendina';
+
+const DropDownList = () => {
+  const classes = useStyles();
+
+  const domande = useSelector(selectData);
+  console.log('ggg', domande);
+  const listItems = domande.map((domanda : any) => (
+
+    /* eslint-disable */
+    <ListItem divider key={domanda.ID}>
+      <Grid container>
+        <Grid item xs={12} sm={8}>
+          <div className={classes.marginTop}>
+            <Typography variant="subtitle1">{domanda.Domanda}</Typography>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <FormControl variant="outlined" fullWidth className={classes.margin}>
+            <InputLabel id="demo-simple-select-outlined-label"> </InputLabel>
+             
+            <ListaATendina id={domanda.ID} />
+          </FormControl>
+        </Grid>
+      </Grid>
+    </ListItem>
+    
+  ));
+  return (
+
+    <div>{listItems}</div>
+
+  );
+};
+
+export default DropDownList;
---
+++ src/component/domandeListaATendina/style.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme:Theme) => ({
+
+  margin: {
+    margin: theme.spacing(3),
+
+  },
+  marginTop: {
+    marginTop: theme.spacing(3),
+
+  },
+
+}));
+export default useStyles;
