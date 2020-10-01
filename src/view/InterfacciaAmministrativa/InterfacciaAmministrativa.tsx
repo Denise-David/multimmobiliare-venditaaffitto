53f57fb272c1dae4ba5e68720773473b101d6693
David Denise <Denise.David@eoc.ch>
2020-10-01 15:56:04 +0200

    aggiunta parti grafiche

--- src/view/InterfacciaAmministrativa/InterfacciaAmministrativa.tsx
+++ src/view/InterfacciaAmministrativa/InterfacciaAmministrativa.tsx
@@ -1,10 +1,28 @@
+import {
+  Card, DialogContent, IconButton, TextField,
+} from '@material-ui/core';
 import React from 'react';
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
 import Navbar from '../../component/Navbar/Navbar';
+import useStyles from './style';
 
-const InterfacciaAmministrativa = () => (
-  <div>
-    <Navbar />
-  </div>
-);
+const InterfacciaAmministrativa = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Navbar />
+      <div className={classes.paddingGeneral}>
+        <Card className={classes.padding}>
+          <TextField className={classes.margin} placeholder="Nome" variant="outlined" />
+          <TextField className={classes.margin} placeholder="Cognome" variant="outlined" />
+          <IconButton color="primary" ari-label="open photocamera" component="span">
+            <PhotoCameraIcon fontSize="large" />
+          </IconButton>
+        </Card>
+        <DialogContent className={classes.listGroup} dividers />
+      </div>
+    </div>
+  );
+};
 
 export default InterfacciaAmministrativa;
