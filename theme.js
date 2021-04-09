186736f284f2bd0746c82c62276e7ffe09de6e17
Denise-David <denise.david@apton.ch>
2021-04-09 07:57:44 +0200

    all

---
+++ theme.js
@@ -0,0 +1,53 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const montserrat = 'Montserrat, sans-serif';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1b1c1d',
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+  },
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+    textTransform: 'none',
+
+  },
+  overrides: {
+    MuiInputBase: {
+      root: {
+        color: 'white',
+
+      },
+      outlined: {
+        color: 'white',
+
+      },
+    },
+
+    MuiOutlinedInput: {
+      notchedOutline: {
+        borderColor: 'white',
+      },
+      root: {
+        Mui: {
+          focused: {
+            borderColor: 'white',
+          },
+        },
+      },
+
+    },
+
+  },
+  MuiCssBaseline: {
+    '@global': {
+      '@font-face': [montserrat],
+    },
+  },
+});
+
+export default theme;
